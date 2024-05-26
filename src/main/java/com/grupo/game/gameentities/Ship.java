40dbdac23208674c11af3b34ac108526ee7a6c9d
Dennis Eckerskorn <denniseckerskorn@gmail.com>
2024-05-26 13:53:32 +0200

    Update ship, not complete

--- src/main/java/com/grupo/game/gameentities/Ship.java
+++ src/main/java/com/grupo/game/gameentities/Ship.java
@@ -4,6 +4,36 @@ import com.grupo.engine.entities.Entity;
 
 public class Ship extends Entity {
 
+    //Damos por hecho que el tamaño del barco es de 3 posiciones(temporalmente para probar)
+    private float pos1;
+    private float pos2;
+    private float pos3;
+    private boolean hit;
+
+    public Ship(float x, float y, float width, float height, float hp, float damage) {
+        super(x, y, width, height, hp, damage);
+        pos1 = x;
+        pos2 = x + width / 2;
+        pos3 = x + width;
+        hit = false;
+    }
+
+    /**
+     * Verifica la posición del golpe en el barco.
+     *
+     * @param hitX Coordenada X del golpe.
+     * @param hitY Coordenada Y del golpe.
+     * @return true si el golpe está dentro del área del barco y el barco no ha sido golpeado.
+     */
+    public boolean checkHitPosition(float hitX, float hitY) {
+        if (!hit) {
+            if (hitX >= pos1 && hitX <= pos3 && hitY == getY()) {
+                hit = true;
+                return true;
+            }
+        }
+        return false;
+    }
 
 
     @Override
