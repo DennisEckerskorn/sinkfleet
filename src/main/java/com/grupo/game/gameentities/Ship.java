f5e3c18f6cab613a3f8c2c9b77c2645dba28b0cf
Dennis Eckerskorn <denniseckerskorn@gmail.com>
2024-05-26 13:21:10 +0200

    Added game package

---
+++ src/main/java/com/grupo/game/gameentities/Ship.java
@@ -0,0 +1,23 @@
+package com.grupo.game.gameentities;
+
+import com.grupo.engine.entities.Entity;
+
+public class Ship extends Entity {
+
+
+
+    @Override
+    public void update(double deltaTime) {
+
+    }
+
+    @Override
+    public void lastUpdate(double deltaTime) {
+
+    }
+
+    @Override
+    public void postUpdate(double deltaTime) {
+
+    }
+}
