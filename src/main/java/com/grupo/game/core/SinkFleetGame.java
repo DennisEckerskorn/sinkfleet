f7abc9198e79454f47e1f07b3a8b79d2025b76ec
15Diego15 <cayodiegodavid@gmail.com>
2024-06-01 02:19:12 +0200

    SinkFleetGame class adapted to Player class

--- src/main/java/com/grupo/game/core/SinkFleetGame.java
+++ src/main/java/com/grupo/game/core/SinkFleetGame.java
@@ -3,7 +3,9 @@ package com.grupo.game.core;
 import com.grupo.engine.core.Blackboard;
 import com.grupo.engine.core.EntityManager;
 import com.grupo.engine.core.Game;
+import com.grupo.engine.input.KeyboardManager;
 import com.grupo.game.config.Settings;
+import com.grupo.game.gameentities.Player;
 
 import java.util.Random;
 
@@ -12,6 +14,9 @@ public class SinkFleetGame extends Game {
     private final int cols;
     private final Random random;
     private final SinkFleetEntityManager sinkFleetEntityManager;
+    private Player player1;
+    private Player player2;
+    private boolean isPlayer1Turn;
 
     public SinkFleetGame(int width, int height, int rows, int cols, float fpsLimit, float updateLimit, int maxEntities) {
         super(width, height, fpsLimit, updateLimit, maxEntities);
@@ -20,6 +25,19 @@ public class SinkFleetGame extends Game {
         random = new Random(); //TODO: No hará falta en el futuro???
         sinkFleetEntityManager = (SinkFleetEntityManager) Blackboard.entityManager;
         spawnShips();
+    }
+
+
+    private void initPlayers() {
+        KeyboardManager km1 = new KeyboardManager('w', 's', 'a', 'd', 'f', ' ');
+        KeyboardManager km2 = new KeyboardManager('i', 'k', 'j', 'l', 'h', ' ');
+        player1 = new Player(0, 0, 0, 0, 100, 0, km1);
+        // Poner coordenadas y medidas, de momento 0
+
+        // TODO: NO estoy seguro del todo si esto esta bien del todo
+        player2 = new Player(0, 0, 0, 0, 100, 0, km2);
+        Blackboard.entityManager.addEntity(player1);
+        Blackboard.entityManager.addEntity(player2);
     }
 
     /**
@@ -50,6 +68,14 @@ public class SinkFleetGame extends Game {
     @Override
     public void update(double deltaTime) {
         super.update(deltaTime);
+        super.update(deltaTime);
+        if (isPlayer1Turn) {
+            player1.procesarInput();
+            player1.update(deltaTime);
+        } else {
+            player2.procesarInput();
+            player2.update(deltaTime);
+        }
     }
 
     @Override
