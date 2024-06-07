b64e4975af02c9aea68f06b8523014c021d13fec
15Diego15 <cayodiegodavid@gmail.com>
2024-06-07 15:09:53 +0200

    Corrections in Player and NPCPlayer class

--- src/main/java/com/grupo/game/gameentities/NPCPlayer.java
+++ src/main/java/com/grupo/game/gameentities/NPCPlayer.java
@@ -1,13 +1,34 @@
 package com.grupo.game.gameentities;
 
-import com.grupo.engine.input.KeyboardManager;
+import java.util.Random;
 
 public class NPCPlayer extends Player {
+    private Random random;
 
     public NPCPlayer(float x, float y, float width, float height, int hp, float damage, int rows, int cols) {
         super(x, y, width, height, hp, damage, null, rows, cols);
+        this.random = new Random();
         //TODO Auto-generated constructor stub
     }
 
-    
+    @Override
+    public void update(double deltaTime) {
+        setActualPostionX(random.nextInt(getRows()));
+        setActualPostionY(random.nextInt(getCols()));
+        setHorizontal(random.nextBoolean());
+    }
+
+    @Override
+    public void lastUpdate(double deltaTime) {
+    }
+
+    @Override
+    public void postUpdate(double deltaTime) {
+    }
+
+    @Override
+    public void processInput() {
+    }
+
+
 }
--- src/main/java/com/grupo/game/gameentities/Player.java
+++ src/main/java/com/grupo/game/gameentities/Player.java
@@ -2,49 +2,53 @@ package com.grupo.game.gameentities;
 
 import com.grupo.engine.entities.PlayableEntity;
 import com.grupo.engine.input.KeyboardManager;
-
-import com.grupo.game.core.BlackBoard2;
 import com.grupo.game.math.Coordinates;
 
 import java.util.ArrayList;
-
 import java.util.List;
+import java.util.Stack;
 
 public class Player extends PlayableEntity {
     private List<Ship> ships;
     private List<Coordinates> disparos;
-    private List<String> stack;
+    private Stack<String> stack;
     private int hp;
     private KeyboardManager keyboardManager;
 
-    //Posicion actual seleccionada por el jugador.
     private float actualPostionX;
     private float actualPostionY;
     private boolean isHorizontal;
-
-    
+    private int rows;
+    private int cols;
 
     public Player(float x, float y, float width, float height, int hp, float damage, KeyboardManager keyboardManager, int rows, int cols) {
         super(x, y, width, height, hp, damage, x, y, y, cols, keyboardManager);
         this.ships = new ArrayList<>(hp);
         this.hp = hp;
         this.disparos = new ArrayList<>();
-        this.stack = new ArrayList<>();
-       
+        this.stack = new Stack<>();
         this.keyboardManager = keyboardManager;
-
         this.actualPostionX = 1;
         this.actualPostionY = 1;
         this.isHorizontal = true;
-
-       
+        this.rows = rows;
+        this.cols = cols;
     }
 
+    public int getRows() {
+        return rows;
+    }
 
+    public int getCols() {
+        return cols;
+    }
 
-    private void disparar(int x, int y) {
-        //TODO: Implementar disparo, es correcto????
-        disparos.add(new Coordinates(x, y));
+    public boolean getIsHorizontal() {
+        return isHorizontal;
+    }
+
+    public void setHorizontal(boolean isHorizontal) {
+        this.isHorizontal = isHorizontal;
     }
 
     public boolean isHitBoard(float x, float y) {
@@ -65,12 +69,11 @@ public class Player extends PlayableEntity {
         }
         return count;
     }
+
     public boolean addShip(Ship ship) {
         return ships.add(ship);
     }
 
-    //#region Getters and Setters
-    
     public List<Ship> getShips() {
         return ships;
     }
@@ -103,63 +106,30 @@ public class Player extends PlayableEntity {
         return keyboardManager;
     }
 
-    //#endregion
-
-    
+    @Override
+    public void update(double deltaTime) {}
 
     @Override
-    public void update(double deltaTime) {
-
-    }
+    public void lastUpdate(double deltaTime) {}
 
     @Override
-    public void lastUpdate(double deltaTime) {
-
-    }
-
-    @Override
-    public void postUpdate(double deltaTime) {
-
-    }
+    public void postUpdate(double deltaTime) {}
 
     @Override
     public void processInput() {
+        if (keyboardManager.isUp()) stack.add("W");
+        if (keyboardManager.isDown()) stack.add("S");
+        if (keyboardManager.isLeft()) stack.add("A");
+        if (keyboardManager.isRight()) stack.add("D");
+        if (keyboardManager.isFire()) stack.add("F");
 
-            if (keyboardManager.isUp())
-                stack.add("W");
-            if (keyboardManager.isDown())
-                stack.add("S");
-            if (keyboardManager.isLeft())
-                stack.add("A");
-            if (keyboardManager.isRight())
-                stack.add("D");
-            if (keyboardManager.isFire())
-                stack.add("F");
-
-            if (stack.size() > 2) {
-                stack.clear();
-            }
-        
+        if (stack.size() > 2) {
+            stack.clear();
+        }
     }
-
-
-
-
 
     @Override
     public String toString() {
-        return "Player [ships=" + ships + ", disparos=" + disparos + ", stack=" + stack + ", hp="
-                + hp + ", keyboardManager=" + keyboardManager + "]";
+        return "Player [ships=" + ships + ", disparos=" + disparos + ", stack=" + stack + ", hp=" + hp + ", keyboardManager=" + keyboardManager + "]";
     }
-
-
-
-
-
-    public boolean getIsHorizontal() {
-        return isHorizontal;
-    }
-
-   
-
 }
